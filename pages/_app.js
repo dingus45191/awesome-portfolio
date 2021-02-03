847fc651ebea7e9cd8be363d22ba0468de16a5de
Mubashir Hasan <mubashirhasan716@gmail.com>
2021-02-03 10:19:00 +0530

    done

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,10 @@
 import '../styles/globals.css'
+import "./App.css"
+import "../public/css/default.css"
+import "../public/css/fonts.css"
+import "../public/css/magnific-popup.css"
+import "../public/css/layout.css"
+import "../public/css/media-queries.css"
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
